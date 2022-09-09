208c2316f88d696b726d47724055f187fa315ab5
Sofie Hemmingsen <sofi413c@stud.kea.dk>
2022-09-09 13:14:04 +0200

    synkroniser med restdb

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const url = "https://passionprojekt-32d5.restdb.io/rest/rejser  "; //endpoint el. URL
-const key = { headers: { "x-apikey": "631b1b2afdc15b0265f17274" } }; // API nøgle
+const url = "https://passionprojekt-32d5.restdb.io/rest/rejser   "; //endpoint el. URL
+const key = { headers: { "x-apikey": "	631b1b2afdc15b0265f17274" } }; // API nøgle
+let food;
+
+async function hentData() {
+  const rensponse = await fetch(url, key);
+  food = await rensponse.json();
+  console.log("FOOD", food);
+  vis();
+}
+hentData(); //loop
